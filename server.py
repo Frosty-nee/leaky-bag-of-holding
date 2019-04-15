2524cb44b0f29f4242fefd3768cf5f19a9332eb0
frosty-nee <frostbite@frosty-nee.net>
2019-04-15 23:58:39 +0000

    got basic file upload working

---
+++ server.py
@@ -0,0 +1,38 @@
+#! python3
+
+import os
+
+import string
+import random
+
+import flask
+from flask import request, session
+
+
+app = flask.Flask(__name__)
+app.secret_key = "key"
+
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    if request.method == 'POST':
+        for f in request.files:
+            file_extension = request.files[f].filename.split('.')[-1]
+            random_filename = ''.join([random.choice(string.ascii_lowercase + string.digits) for n in range(12)]) + '.' + file_extension
+            request.files[f].save(os.path.join('uploads/', random_filename))
+        return 'https://file.frosty-nee.net/' + random_filename
+    else:
+        return flask.render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return flask.render_template('login.html')
+    
+    else:
+        return
+@app.route('/account')
+def account():
+    return flask.render_template('account.html')
+
+if __name__ == '__main__':
+    app.run()
